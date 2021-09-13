b6795af975588a442621cafea1161c3f9fb77e4e
codma <ruddud1123@naver.com>
2021-09-13 19:02:46 +0900

    store setting using saga

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './pages/Home'
+import Add from './pages/Add'
+import Signin from './pages/Signin'
+import Edit from './pages/Edit'
+import Detail from './pages/Detail'
+import NotFound from './pages/NotFound'
+import Error from './pages/Error'
+import { ErrorBoundary } from 'react-error-boundary';
+
+function App() {
+  return (
+    <ErrorBoundary FallbackComponent={Error}>
+          <BrowserRouter>
+    <Switch>
+      <Route exact path="/edit/:id" component={Edit} />
+      <Route exact path="/book/:id" component={Detail} />
+      <Route exact path="/add" component={Add} />
+      <Route exact path="/siginin" component={Signin} />
+      <Route exact path="/" component={Home} />
+      <Route component={NotFound}/>
+    </Switch>
+    </BrowserRouter>
+    </ErrorBoundary>
+
+  );
+}
+
+export default App;
